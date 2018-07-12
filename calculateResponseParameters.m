fd1fceee21b5c1bbb71226a6b99c61dd270aa6a6
John Barrett <barrett.m.john@googlemail.com>
2018-07-11 19:33:45 -0500

    add some plots to response parameter calculation

--- calculateResponseParameters.m
+++ calculateResponseParameters.m
@@ -1,4 +1,4 @@
-function [volume,peak,peakIndex,latencyIndex] = calculateResponseParameters(mpsth,params,psthFigure)
+function [volume,peak,peakIndex,latencyIndex] = calculateResponseParameters(mpsth,params,psthFigure,isPlot)
     stimulusIndex = 101; % TODO : specify?
     baseline = mpsth(1:(stimulusIndex-1),:,:);
     % TODO : per probe or even per channel baseline is better but 
@@ -34,4 +34,40 @@ function [volume,peak,peakIndex,latencyIndex] = calculateResponseParameters(mpst
     peakIndex = squeeze(peakIndex);
     
     latencyIndex = squeeze(cellfun(@(r) sum(find(r > mu+3*sigma,1)),mat2cell(responses,size(responses,1),ones(1,size(responses,2)),ones(1,size(responses,3)))));
+    
+    if (nargin > 4 && ~isPlot) || (nargin > 3 && islogical(psthFigure) && ~psthFigure)
+        return % plot by default
+    end
+    
+    figure
+    datas = {volume peak peakIndex/1000 latencyIndex/1000};
+    dataLabels = {'Response Volume' 'Response Peak' 'Peak Latency' 'Threshold Latency'};
+    nDatas = numel(datas);
+    nProbes = size(volume,1);
+    
+    for ii = 1:nDatas
+        clim = [min(datas{ii}(isfinite(datas{ii}))) max(datas{ii}(isfinite(datas{ii})))];
+        
+        if clim(1) == clim(2)
+            clim(2) = clim(1)+1;
+        end
+        
+        for jj = 1:nProbes
+            subplot(nDatas,nProbes,nProbes*(ii-1)+jj);
+            
+            imagesc(createMap(datas{ii}(jj,:),params));
+            
+            caxis(clim);
+            colorbar;
+            colormap(hot);
+            
+            if ii == 1
+                title(sprintf('Probe %d',jj));
+            end
+            
+            if jj == 1
+                ylabel(dataLabels{ii});
+            end
+        end
+    end
 end
