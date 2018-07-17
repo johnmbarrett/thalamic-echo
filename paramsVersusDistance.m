75bdd7d971df06a8804b7f0e10b0228c1a7ac98f
John Barrett <barrett.m.john@googlemail.com>
2018-07-17 12:08:43 -0500

    params versus distance for all datasets

--- paramsVersusDistance.m
+++ paramsVersusDistance.m
@@ -1,10 +1,15 @@
+% TODO : laterality corrected distance, gordon's plots
+
 topDir = 'Z:\LACIE\Manuscripts\2018 in vivo LSPS Ntsr1 etc\data';
 cd(topDir);
 
 load('probe_locations.mat');
 
-
 %%
+
+slopes = cellfun(@(A) cell(size(A)),mps,'UniformOutput',false);
+intercepts = cellfun(@(A) cell(size(A)),mps,'UniformOutput',false);
+R2s = cellfun(@(A) cell(size(A)),mps,'UniformOutput',false);
 
 for gg = 1:numel(probeLocations)
     for hh = 1:numel(mps{gg})
@@ -54,10 +59,11 @@ for gg = 1:numel(probeLocations)
             daspect([1 1 1]);
         end
         
+        d = max(dd(:));
+        m = ceil(size(dd,2)/2);
+        
         jbsavefig(gcf,'all_maps_%s_%s',dates(gg,:),mps{gg}(hh).name);
         close(gcf);
-        
-        continue
 
         %%
 
@@ -82,18 +88,20 @@ for gg = 1:numel(probeLocations)
                 y = cell(1,2);
 
                 for kk = 1:2
-                    x{kk} = reshape(dd(:,(6*(kk-1)+1):6*kk,probes(jj)),[],1);
-                    y{kk} = reshape(datas{ii}(:,(6*(kk-1)+1):6*kk,probes(jj)),[],1);
+                    x1 = 1+(kk-1)*ceil(m);
+                    x2 = ceil(m)*kk-mod(size(dd,2),2);
+                    x{kk} = reshape(dd(:,x1:x2,probes(jj)),[],1);
+                    y{kk} = reshape(datas{ii}(:,x1:x2,probes(jj)),[],1);
                     plot(x{kk},y{kk},'Color',[kk-1 0 2-kk],'LineStyle','none','Marker','o');
                     [beta(ii,jj,kk,:),~,~,~,stats] = regress(y{kk}(isfinite(y{kk})),[ones(sum(isfinite(y{kk})),1) x{kk}(isfinite(y{kk}))]);
-                    plot(0:10,beta(ii,jj,kk,1)+(0:10)*beta(ii,jj,kk,2),'Color',3*[kk-1 0 2-kk]/4);
+                    plot(0:d,beta(ii,jj,kk,1)+(0:d)*beta(ii,jj,kk,2),'Color',3*[kk-1 0 2-kk]/4);
                     R2(ii,jj,kk) = stats(4);
                 end
 
                 x = vertcat(x{:});
                 y = vertcat(y{:});
                 [beta(ii,jj,3,:),~,~,~,stats] = regress(y(isfinite(y)),[ones(sum(isfinite(y)),1) x(isfinite(y))]);
-                plot(0:10,beta(ii,jj,3,1)+(0:10)*beta(ii,jj,3,2),'Color',[0.75 0 0.75]);
+                plot(0:d,beta(ii,jj,3,1)+(0:d)*beta(ii,jj,3,2),'Color',[0.75 0 0.75]);
                 R2(ii,jj,3) = stats(4);
                 
                 ylim(yy);
@@ -107,5 +115,18 @@ for gg = 1:numel(probeLocations)
                 end
             end
         end
+        
+        slopes{gg}{hh} = beta(:,:,:,2);
+        intercepts{gg}{hh} = beta(:,:,:,2);
+        R2s{gg}{hh} = R2;
+        
+        jbsavefig(gcf,'responses_versus_distance_%s_%s',dates(gg,:),mps{gg}(hh).name);
+        close(gcf);
     end
 end
+
+cd(topDir);
+
+comment = sprintf('Slope is in ms/mm\n\nOuter cell array is date; inner cell array is multiple recordings on the same day; rows are latency, then peak, then vol; columns are probes; pages are left hemisphere, then right, then bilateral');
+
+save('spread_parameters','comment','dates','mps','slopes','intercepts','R2s');
